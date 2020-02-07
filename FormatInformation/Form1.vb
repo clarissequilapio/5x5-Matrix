7a9716682ebb996300f82c4edae7c2a95681bcf4
clarissequilapio <clairequilapio@gmail.com>
2020-02-07 16:44:56 +0800

    variables set

--- FormatInformation/Form1.vb
+++ FormatInformation/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
+    Private Sub nameInfo_TextChanged(sender As Object, e As EventArgs) Handles nameInfo.TextChanged
 
+
+    End Sub
+
+    Private Sub ageInfo_TextChanged(sender As Object, e As EventArgs) Handles ageInfo.TextChanged
+        Dim ageInfo As Byte
+
+    End Sub
+
+    Private Sub addressInfo_TextChanged(sender As Object, e As EventArgs) Handles addressInfo.TextChanged
+        Dim addressInfo As String
+
+    End Sub
+
+    Private Sub nameLabel_Click(sender As Object, e As EventArgs) Handles nameLabel.Click
+
+    End Sub
 End Class
